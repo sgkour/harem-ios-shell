e7500ec1fe61739589ab9a8dc5a111e33f344835
sgkour <lxhywan1@163.com>
2026-08-14 01:22:06 +0800

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.harem',
+  appName: 'HaremGame',
+  webDir: 'www'
+};
+
+export default config;
